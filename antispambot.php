dc22c74373ff8060d58d22396b1567250055961f
Chris Ferdinandi <hello@gomakethings.com>
2014-11-21 22:47:59 -0500

    Removed trailing ?>

--- antispambot.php
+++ antispambot.php
@@ -1,27 +1,23 @@
 <?php
 
-/* ======================================================================
+/**
+ * Plugin Name: WP Email Antispambot
+ * Plugin URI: https://github.com/cferdinandi/antispambot/
+ * Description: Provides a shortcode for the antispambot function that's built into WordPress.
+ * Version: 1.1.0
+ * Author: Chris Ferdinandi
+ * Author URI: http://gomakethings.com
+ * License: MIT
+ *
+ * Learn more in the WordPress Codex.
+ * http://codex.wordpress.org/Protection_From_Harvesters
+ */
 
-	Plugin Name: WP Email Antispambot
-	Plugin URI: https://github.com/cferdinandi/antispambot/
-	Description: Provides a shortcode for the antispambot function that's built into WordPress.
-	Version: 1.1
-	Author: Chris Ferdinandi
-	Author URI: http://gomakethings.com
-	License: MIT
-
-	Learn more in the WordPress Codex.
-	http://codex.wordpress.org/Protection_From_Harvesters
-
- * ====================================================================== */
-
-function emailasb_antispambot( $atts ) {
-	extract(shortcode_atts(array(
-		'email' => '',
-	), $atts));
-	$antispambot = antispambot( $email );
-	return $antispambot;
-}
-add_shortcode( 'email_antispambot', 'emailasb_antispambot' );
-
-?>
+	function emailasb_antispambot( $atts ) {
+		extract(shortcode_atts(array(
+			'email' => '',
+		), $atts));
+		$antispambot = antispambot( $email );
+		return $antispambot;
+	}
+	add_shortcode( 'email_antispambot', 'emailasb_antispambot' );
